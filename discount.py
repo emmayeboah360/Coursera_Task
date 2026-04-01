c7bf3bf9ea8c381c7483a7e5e5f1634ae155d484
Emmanuel Yeboah <154805868+emmayeboah360@users.noreply.github.com>
2026-04-01 15:44:18 +0000

    Update discount.py

--- discount.py
+++ discount.py
@@ -1,1 +1,10 @@
+#Original price given by the task
+original_price = 75
 
+#finding for the discounted amount
+discount = (15/100) * original_price
+
+#The new price after the discount
+final_price = original_price - discount
+
+print(final_price)
